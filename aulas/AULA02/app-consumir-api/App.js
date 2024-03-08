d159f605bb57b4996c89fd205f4690520861191b
Jorge S. <you@example.com>
2024-03-08 19:27:33 -0300

    atualização de pastas

--- aulas/AULA02/app-consumir-api/App.js
+++ aulas/AULA02/app-consumir-api/App.js
@@ -1,17 +1,53 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Alert
+} from "react-native";
 
 import api from "./src/services/api/api";
 
 export default function App() {
   const [cliente, setCliente] = useState([]);
+  const [idCli, setIdCli] = useState(0);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const getCliente = async () => {
+  const getCliente = async (id) => {
     try {
-      const { data } = await api.get(`/clientes/17`);
-      console.log(data)
-      setCliente(data);
+      if (id > 0) {
+        const response = { data } = await api.get(`/clientes/${id}`)
+          .catch(function (error) {
+            if (error.response) {
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.resquest) {
+              if (error.resquest._response.includes("Falied")) {
+                console.log("Erro ao conectar com a API");
+              }
+            } else {
+              console.log("Error", error.message);
+            }
+            console.log(error.mensagem)
+          });
+
+          if (response != undefined) {
+            if(response.data.length === 0){
+              setCliente([])
+              setShowAlert(true)
+            }else {
+              setCliente(response.data)
+            }
+          }
+
+      } else {
+        setCliente([]);
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -20,23 +56,36 @@ export default function App() {
   return (
     <View style={styles.container}>
       <TextInput
-      style={styles.texInput}
-      >
+        style={styles.texInput}
+        placeholder="ID Cliente"
+        keyboardType="numeric"
+        onChangeText={setIdCli}
+        value={idCli}
+      ></TextInput>
 
-      </TextInput>
-
-
-      <TouchableOpacity 
-      onPress={() => getCliente()} 
-      style={styles.botao}
-      >
+      <TouchableOpacity onPress={() => getCliente(idCli)} style={styles.botao}>
         <Text style={{ color: "white" }}>pressione para pesquisar</Text>
       </TouchableOpacity>
 
-      <Text>ID do cliente: {cliente[0]?.id} </Text>
-      <Text>Nome do cliente: {cliente[0]?.nome} </Text>
-      <Text>Idade do cliente: {cliente[0]?.idade} </Text>
+      <Text>ID: </Text>
+      <TextInput
+        style={styles.texInput}
+        value={cliente[0]?.id.toString()}
+      ></TextInput>
+      <Text>Nome: </Text>
+      <TextInput style={styles.texInput} value={cliente[0]?.nome}></TextInput>
+      <Text>Idade: </Text>
+      <TextInput
+        style={styles.texInput}
+        value={cliente[0]?.idade.toString()}
+      ></TextInput>
 
+      {showAlert &&
+        Alert.alert(
+          "Informação", "Esse registro não foi localizado na base de dados",
+          [
+            { text: "ok", onPress: () => setShowAlert(false) }
+          ])}
       <StatusBar style="auto" />
     </View>
   );
@@ -50,18 +99,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   botao: {
-    alingItens: "center",
+    alignItems: "center",
     justifyContent: "center",
-    width: "80%",
+    width: "70%",
     height: 40,
     borderRadius: 4,
     backgroundColor: "red",
   },
-  texinput:{
+  texInput: {
+    width: "80%",
+    height: 40,
     borderWidth: 1,
     borderColor: "black",
     borderRadius: 5,
     padding: 5,
+    marginBottom: 10,
+    textAlign: "center",
   },
-
 });
