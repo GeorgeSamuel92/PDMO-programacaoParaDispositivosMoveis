94e1fe4f4476fa4b377a17b8b7006d6439722d1e
Jorge S. <you@example.com>
2024-03-21 21:42:40 -0300

    Criação do banco de dados do app

--- aulas/AULA03/app-bd-sqlite/App.js
+++ aulas/AULA03/app-bd-sqlite/App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+import { useState, useEffect } from "react";
+import { DatabaseConnection } from "./src/database/database";
+
+const db = new DatabaseConnection.getConnection();
 
 export default function App() {
+  const [nome, setNome] = useState(null);
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS clientes (id INTERGER PRIMARY KEY AUTOINCREMENT, nome TEXT)",
+        [],
+        () => console.log("Tabela clientes criada com sucesso"),
+      );
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Cadastro</Text>
+      <TextInput
+        style={styles.input}
+        value={nome}
+        onChangeText={setNome}
+        placeholder="Digite um nome"
+      />
+
+      <Button title="Cadastrar" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "90%",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "black",
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
